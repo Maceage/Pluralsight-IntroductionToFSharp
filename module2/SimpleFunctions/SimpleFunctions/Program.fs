1b1c21ccf2e2ca4f8fd7eca47366c1a2897b9daa
Graham Mace <graham@maceage.com>
2016-01-08 12:53:58 +1100

    Added SimpleFunctions

---
+++ module2/SimpleFunctions/SimpleFunctions/Program.fs
@@ -0,0 +1,15 @@
+﻿let square x = x * x
+
+printfn "square 5 is %d" (square 5)
+
+let add x y =
+    x + y
+
+let add' x y =
+    let result =
+        x + y
+    result
+
+let add5and3 = add 5 3
+
+let result = add (square 12) 7
