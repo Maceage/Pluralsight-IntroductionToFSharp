5885bda42fde61453567ade0a4bc55564360a221
Graham Mace <graham@maceage.com>
2016-01-12 09:58:24 +1100

    Added DeclaringFunctionsAndLambdaExpressions

---
+++ module4/DeclaringFunctionsAndLambdaExpressions/DeclaringFunctionsAndLambdaExpressions/Program.fs
@@ -0,0 +1,12 @@
+﻿let add x y = x + y
+
+let add' = fun x y -> x + y
+
+let checkThis (item: 'c) (f: 'c -> bool) : unit = 
+    if f item then
+        printfn "HIT"
+    else
+        printfn "MISS"
+
+checkThis 5 (fun x -> x > 3)
+checkThis "hi there" (fun x -> x.Length > 5)
