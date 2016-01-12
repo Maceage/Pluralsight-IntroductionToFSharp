98969c25597ef4dcff3ec5dd52841b88d31f3b2d
Graham Mace <graham@maceage.com>
2016-01-12 12:03:52 +1100

    Added Precomputation

---
+++ module4/Precomputation/Precomputation/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Precomputation.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Precomputation")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Xero")>]
+[<assembly: AssemblyProduct("Precomputation")>]
+[<assembly: AssemblyCopyright("Copyright © Xero 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("c05ac195-c822-4755-a194-318c65d6001d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ module4/Precomputation/Precomputation/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.Collections.Generic
+
+let isInList (list: 'a list) v =
+    let lookupTable = new HashSet<'a>(list)
+    printfn "Lookup table created, looking up value"
+    lookupTable.Contains v
+
+printfn "%b" (isInList ["hi"; "there"; "oliver"] "there")
+printfn "%b" (isInList ["hi"; "there"; "oliver"] "anna")
+
+let isInListClever = isInList ["hi"; "there"; "oliver"]
+printfn "%b" (isInListClever "there")
+printfn "%b" (isInListClever "anna")
+
+
+let constructLookup (list: 'a list) =
+    let lookupTable = new HashSet<'a>(list)
+    printfn "Lookup table created"
+    fun v ->
+        printfn "Performing lookup"
+        lookupTable.Contains v
+
+let isInListClever' = constructLookup ["hi"; "there"; "oliver"]
+printfn "%b" (isInListClever' "there")
+printfn "%b" (isInListClever' "anna")
