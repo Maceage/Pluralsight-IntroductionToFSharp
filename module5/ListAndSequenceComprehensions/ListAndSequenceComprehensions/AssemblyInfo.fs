7192e5d94cb7828e1e3b353c7241a84879579cc9
Graham Mace <graham@maceage.com>
2016-01-12 17:22:11 +1100

    Added ListAndSequenceComprehensions

---
+++ module5/ListAndSequenceComprehensions/ListAndSequenceComprehensions/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ListAndSequenceComprehensions.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ListAndSequenceComprehensions")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Xero")>]
+[<assembly: AssemblyProduct("ListAndSequenceComprehensions")>]
+[<assembly: AssemblyCopyright("Copyright © Xero 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("d80af783-cbb6-452d-b8d5-118b815f9cdf")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ module5/ListAndSequenceComprehensions/ListAndSequenceComprehensions/Program.fs
@@ -0,0 +1,37 @@
+﻿let output x = printfn "%A" x
+
+let ints = [7..13]
+output ints
+
+let oddValues = [1..2..20]
+output oddValues
+
+let values step max = [1..step..max]
+output (values 2 20)
+
+let ints' = seq { 7..13 }
+output ints'
+
+output [ for x in 7..13 -> x, x * x ]
+output [ for x in 7..13 ->
+                            printfn "Return new value now"
+                            x, x * x ]
+
+let yieldedValues =
+    seq {
+        yield 3;
+        yield 42;
+        for i in 1..3 do
+            yield i
+        yield! [5; 7; 8]
+    }
+
+output (List.ofSeq yieldedValues)
+
+let yieldedStrings =
+    seq {
+        yield "this"
+        yield "that"
+    }
+
+output yieldedStrings
