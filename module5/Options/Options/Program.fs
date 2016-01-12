b98ed9304ab0ff540190175d8fdff049b44d3104
Graham Mace <graham@maceage.com>
2016-01-12 12:20:51 +1100

    Added Options

---
+++ module5/Options/Options/Program.fs
@@ -0,0 +1,16 @@
+﻿let o1 = Some(5)
+let o2 = None
+
+if o1 = o2 then
+    printfn "Values are equal"
+
+printfn "o1 IsSome: %b" o1.IsSome
+printfn "o2 IsNone: %b" o2.IsNone
+
+let checkOption o =
+    match o with
+    | Some(x) -> printfn "Options contains value %A" x
+    | None -> printfn "Option doesn't have a value"
+
+checkOption o1
+checkOption o2
