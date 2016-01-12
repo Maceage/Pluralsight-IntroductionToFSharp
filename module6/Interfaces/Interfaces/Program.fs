31c622a7152d55e8fa5386a86d66c8dc2caa6702
Graham Mace <graham@maceage.com>
2016-01-12 18:38:07 +1100

    Added Interfaces

---
+++ module6/Interfaces/Interfaces/Program.fs
@@ -0,0 +1,24 @@
+﻿type IMyInterface =
+    abstract member Value: int with get
+
+type IDerivedInterface =
+    inherit IMyInterface
+
+    abstract member Add: int -> int -> int
+
+type MyClass() =
+    interface IMyInterface with
+        member x.Value with get() = 13
+
+type MyOtherClass() =
+    member this.Add x y = x + y
+
+    interface IDerivedInterface with
+        member i.Add x y = i.Add x y
+        member x.Value = 42
+
+let moc = MyOtherClass()
+
+printfn "%A" (moc.Add 10 20)
+printfn "%A" ((moc :> IMyInterface).Value)
+printfn "%A" ((moc :> IDerivedInterface).Add 10 20)
