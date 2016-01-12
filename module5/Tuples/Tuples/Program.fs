70b629f12b1e87d3541a9b2fdb7bcc42719666b5
Graham Mace <graham@maceage.com>
2016-01-12 12:17:34 +1100

    Added Tuples

---
+++ module5/Tuples/Tuples/Program.fs
@@ -0,0 +1,16 @@
+﻿let t1 = 12, 5, 7
+let v1, v2, _ = t1
+
+let t2 = "hi", true
+
+printfn "%A" (fst t2)
+printfn "%A" (snd t2)
+
+let third t =
+    let _, _, r = t
+    r
+
+printfn "%A" (third t1)
+
+let third' (_, _, r) = r
+printfn "%A" (third' t1)
