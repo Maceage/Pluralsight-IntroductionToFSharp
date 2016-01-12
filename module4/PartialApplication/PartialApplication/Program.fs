07b78e80c415a5bf476cd5822ff95e70a786c8ce
Graham Mace <graham@maceage.com>
2016-01-12 11:29:42 +1100

    Added PartialApplication

---
+++ module4/PartialApplication/PartialApplication/Program.fs
@@ -0,0 +1,20 @@
+﻿let add x y = x + y
+let mult x y = x * y
+let square x = x * x
+
+let add' = fun x y -> x + y
+// (int -> int -> int)
+let add'' x = fun y -> x + y
+// int -> (int -> int)
+
+let add10'' = add'' 10
+printfn "%d" (add10'' 32)
+
+let add10 = add 10
+printfn "%d" (add10 32)
+
+let addmultiplevalues a b c d = a + b + c + d
+
+let result = addmultiplevalues 10
+
+let result2 = result 15
