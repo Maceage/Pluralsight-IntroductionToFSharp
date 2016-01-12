faab5efc9b3e64786bee95510068811ba5ee07e6
Graham Mace <graham@maceage.com>
2016-01-12 19:00:55 +1100

    Added IDisposableHandling

---
+++ module7/IDisposableHandling/IDisposableHandling/Program.fs
@@ -0,0 +1,15 @@
+﻿let createDisposable f =
+    { new System.IDisposable with member x.Dispose() = f() }
+
+let outerFunction() =
+    use disposable = createDisposable (fun () -> printfn "Now disposing of myself")
+    printfn "In outer function"
+
+outerFunction()
+
+let outerFunction'() =
+    using (createDisposable (fun () -> printfn "Now disposing of myself"))
+        (fun d -> printfn "Acting on the disposable object now")
+    printfn "In outer function '"
+
+outerFunction'()
