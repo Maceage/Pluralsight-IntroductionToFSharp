1b5c45dc6b0db4649932dc6ea4cd323672847a00
Graham Mace <graham@maceage.com>
2016-01-12 12:12:28 +1100

    Added Recursion

---
+++ module4/Recursion/Recursion/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Threading
+
+let rec fact x =
+    if x = 1 then 1
+    else x * (fact (x - 1))
+
+printfn "%d" (fact 5)
+
+let rec fnA() = fnB()
+and fnB() = fnA()
+
+let showValues() =
+    let r = Random()
+    let rec dl() =
+        printfn "%d" (r.Next())
+        Thread.Sleep(1000)
+        dl()
+
+    dl() // Will result in a stack overflow due to method calls
+
+//    while true do
+//        printfn "%d" (r.Next())
+//        Thread.Sleep(1000)
+
+showValues()
